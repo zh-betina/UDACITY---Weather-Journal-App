cf977981a26e9cf6aec44ba313ffc61197a1d31f
Betina Jasinska <zh.betina@gmail.com>
2020-04-18 17:42:28 +0200

    Fix

--- website/app.js
+++ website/app.js
@@ -1,6 +1,8 @@
+/*User input*/
 let zip = document.getElementById('zipcode').value;
 let place = document.getElementById('country').value;
 let feel = document.getElementById('feelings').value;
+
 /*API credentials*/
 const baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
 const apiKey = '&appid=51a1e583a80570b3b8cd3b2021b86cac';
@@ -8,8 +10,8 @@ const units = '&units=metric'
 let url = `${baseUrl}zip=${zip},${place}${apiKey}${units}`;
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const date = new Date();
+const newDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 
 
 //'Generate' event listener
@@ -18,21 +20,23 @@ document.getElementById('generate').addEventListener('click', getInput);
 //Chained promises
 //1)GET data from the API,  2)POST that data to server, 3)Update the UI
 function getInput(e){
-  zip = document.getElementById('zipcode').value;
-  place = document.getElementById('country').value;
-  feel = document.getElementById('feelings').value;
+zip = document.getElementById('zipcode').value;
+place = document.getElementById('country').value;
+feel = document.getElementById('feelings').value;
+
   url = `${baseUrl}zip=${zip},${place}${apiKey}${units}`;
   getApiData(url)
-  .then(function(data){
-    console.log(data)
-    postData('/add', {"base": data.base})
+  .then(function(newData){
+    console.log(newData)
+    postData('/add', {temperature: newData.main.temp, feelings: feel, date: newDate});
     })
   .then(function(postData){
     updateUI();
   });
 };
 
-//GET weather info from the API
+
+//GET weather info from the OpenWeatherApi
 const getApiData =  async(url)=>{
   const apiResponse = await fetch(url);
     try {
@@ -54,13 +58,12 @@ const postData = async(url = "/add", data = {})=>{
     },
     body: JSON.stringify(data),
   });
-
   try{
     const newData = await response.json();
     console.log(newData);
     return newData;
   }catch(error){
-    console.log('While POSTing weather data to the server an error occured: ', error);
+    console.log('While POSTing weather and user input data to the server an error occured: ', error);
     }
 };
 
@@ -71,7 +74,9 @@ const updateUI = async(url = "/entries")=>{
     try{
       const entries = await getEntries.json()
       console.log(entries);
-      document.getElementById('content').innerHTML = entries[0].base;
+      document.getElementById('date').innerHTML = `<span class="fas fa-calendar-day"></span> ${entries[0].date}`;
+      document.getElementById('temp').innerHTML = `<span class="fas fa-temperature-high"></span> ${entries[0].temperature}°C`;
+      document.getElementById('content').innerHTML = `<span class="fas fa-thumbtack"></span> ${entries[0].feelings}`;
     }catch(error){
       console.log('While updating UI an error occured: ', error)
     };
